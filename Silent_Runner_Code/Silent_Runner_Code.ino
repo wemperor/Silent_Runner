dc8aa497ada853125e5b23e2b1a57309b112bc55
wemperor <wemperor@gmail.com>
2014-06-25 10:00:23 +0200

    Update Silent_Runner_Code.ino
    Change to interrupt based readings
    Works way faster
    tested - working

--- Silent_Runner_Code/Silent_Runner_Code.ino
+++ Silent_Runner_Code/Silent_Runner_Code.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+// download at https://github.com/GreyGnome/PinChangeInt/archive/master.zip
+#include <PinChangeInt.h>
 
 //defines the servo object
 Servo ServoL;  //Servo fin stern left
@@ -13,33 +15,38 @@ Servo ServoU;  //Servo fin stern up
 #define ServoRpin 6 //Servo fin stern right
 #define ServoUpin 9 //Servo fin stern up
 
-           
-
 //variables for the maneuver calculations
-double Rotation;
-double Xaxis; 
+int Rotation;
+int Xaxis; 
+
+// interrupt stuff
+volatile uint8_t updateFlagsShared;
+
+volatile uint16_t vRotation;
+volatile uint16_t vXaxis;
+
+uint32_t ulRotationStart;
+uint32_t ulXaxisStart;
+
+#define ROTATION_FLAG 1
+#define XAXIS_FLAG 2
 
 
-int ServoLout = 90; //Servo fin stern left
-int ServoRout = 90; //Servo fin stern right
-int ServoUout = 90; //Servo fin stern up
+uint8_t updateChannel;
+
+int ServoLout = 1500; //Servo fin stern left
+int ServoRout = 1500; //Servo fin stern right
+int ServoUout = 1500; //Servo fin stern up
 
 //max range of the servos
-#define MIN_A 10
-#define MAX_A 170
+
 #define MAX_DEG 500  
-#define MAX_DEG_SQUARE 250000
 #define MIN_PWM 1500
 
 // divisor for correcting rudder 
 #define YCORRECTDIV 2
+#define UPCORRECTDIV 0.7
 
-// stuff for value correction via averaging
-#define NUMREADINGS 8 //chose high values for smoother but slower control
-
-int readings_CH2[ NUMREADINGS ] ;
-int readings_CH3[ NUMREADINGS ] ;
-int index = 0;  
 
 void setup()
 {
@@ -50,56 +57,103 @@ void setup()
 
   pinMode(CH2pin, INPUT);
   pinMode(CH3pin, INPUT);
+  
+  Xaxis = 0;
+  Rotation = 0;
+  
 
-  //setting the smoothing arrays to zero
-  for (byte thisReading = 0; thisReading < NUMREADINGS; thisReading++) {
-    readings_CH2[thisReading] = 0.0;
-    readings_CH3[thisReading] = 0.0;
-  }
+  PCintPort::attachInterrupt(CH2pin, calcRotation,CHANGE);
+  PCintPort::attachInterrupt(CH3pin, calcXaxis,CHANGE);
+  
 } //setup end
 
 void loop()
 {
-  Rotation -= readings_CH2[index];
-  Xaxis -= readings_CH3[index];
-
-  // values range from -500 to 500
-  int Rotation_tmp = -(((long)pulseIn(CH2pin, HIGH))-MIN_PWM); 
-  int Xaxis_tmp = (((long)pulseIn(CH3pin, HIGH))-MIN_PWM); 
-
-  Rotation += Rotation_tmp;
-  Xaxis += Xaxis_tmp;
-
-  readings_CH2[index] = Rotation_tmp;
-  readings_CH3[index] = Xaxis_tmp;
-  index = (index + 1) % NUMREADINGS;
-
-   /* Control Matrix for inverted Y fins
+  static uint16_t vRotationIn;
+  static uint16_t vXaxisIn;
    
-               | Rotation  |  Xaxis  |
-   ServoLeft   |    +      |    +    |
-   ServoRight  |    +      |    -    |
-   ServoUp     |    -      |   N/A   |     */
-  
-  ServoLout = ((br(Rotation) / YCORRECTDIV) + cubic(br(Xaxis))); //calculation of maneuvers
-  ServoRout = ((br(Rotation) / YCORRECTDIV) - cubic(br(Xaxis)));
-  ServoUout = (-br(Rotation));
+  static uint8_t updateFlags;
 
-  ServoL.write(forServo(ServoLout));
-  ServoR.write(forServo(ServoRout));
-  ServoU.write(forServo(ServoUout));
+  if(updateFlagsShared)
+  {
+    noInterrupts(); 
+
+    
+    updateFlags = updateFlagsShared;
+    if(updateFlags & ROTATION_FLAG)
+    {
+      vRotationIn = vRotation;
+    }
+
+    if(updateFlags & XAXIS_FLAG)
+    {
+      vXaxisIn = vXaxis;
+    }
+  
+    updateFlagsShared = 0;
+
+    interrupts(); 
+  }
+
+  if(updateFlags & ROTATION_FLAG) {
+    Rotation = -(vRotationIn-MIN_PWM); 
+    
+    updateChannel |= ROTATION_FLAG;
+  }
+
+  if(updateFlags & XAXIS_FLAG) {
+    Xaxis = (vXaxisIn-MIN_PWM);
+
+    updateChannel |= XAXIS_FLAG;
+  }
+  
+  updateFlags = 0;
+  
+  updateChannel = 0;
+
+    /* Control Matrix for inverted Y fins
+     
+     | Rotation  |  Xaxis  |
+     ServoLeft   |    +      |    +    |
+     ServoRight  |    +      |    -    |
+     ServoUp     |    -      |   N/A   |     */
+
+    ServoLout = (Rotation / YCORRECTDIV) + Xaxis; //calculation of maneuvers
+    ServoRout = (Rotation / YCORRECTDIV) - Xaxis;
+    ServoUout = -(Rotation / UPCORRECTDIV);
+
+    ServoL.writeMicroseconds(forServo(ServoLout));
+    ServoR.writeMicroseconds(forServo(ServoRout));
+    ServoU.writeMicroseconds(forServo(ServoUout));
+  
 
 } //loop end
 
-// cubic function to fit into [MIN_DEG, MAX_DEG]
-inline int cubic(double in) {
- return (in * in * in) / MAX_DEG_SQUARE;
-}
-// by readings 
-inline int br(double in) {
-  return in / NUMREADINGS;
+
+// interrupt routines
+void calcRotation() {
+  if(digitalRead(CH2pin) == HIGH)
+  { 
+    ulRotationStart = micros();
+  }
+  else
+  { 
+    vRotation = (uint16_t)(micros() - ulRotationStart);
+    updateFlagsShared |= ROTATION_FLAG;
+  }
 }
 
+void calcXaxis() {
+  if(digitalRead(CH3pin) == HIGH)
+  { 
+    ulXaxisStart = micros();
+  }
+  else
+  { 
+    vXaxis = (uint16_t)(micros() - ulXaxisStart);
+    updateFlagsShared |= XAXIS_FLAG;
+  }
+}
 
 inline long forServo(int s) {
   if (s >= MAX_DEG) {
@@ -108,5 +162,6 @@ inline long forServo(int s) {
   if (s <= -MAX_DEG) {
     s = -MAX_DEG;
   } //setÂ´s -MAX_DEG as minimum for the servos
-  return map(s,-MAX_DEG,MAX_DEG,MIN_A,MAX_A); //maps the values from -90 to 90 to the necessary output 10 to 170
+  return s + MIN_PWM; //maps the values from -90 to 90 to the necessary output 10 to 170
 }
+
